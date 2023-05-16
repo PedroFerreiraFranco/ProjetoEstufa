4c0d78882be494551a366e5a95a1b0425937a2b1
Pedro Franco <101286154+PedroFerreiraFranco@users.noreply.github.com>
2023-05-16 15:34:01 -0300

    Add files via upload

--- Estufa.ino
+++ Estufa.ino
@@ -1,3 +1,8 @@
+#include <dht11.h>
+#define DHT11PIN 10
+
+dht11 DHT11;
+
 int umidade;
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 13, 7, 6, 5, 4);
@@ -12,32 +17,48 @@ void setup()
  lcd.begin(16, 2);
  pinMode(porta_rele, OUTPUT);
 }
-void loop()
-{
- umidade = analogRead(A0);
- int Porcento = map(umidade, 1023, 0, 0, 100);
-
-  lcd.clear();
- Serial.print(Porcento);
- Serial.println("%");
- if(Porcento <=40)
- {
- Serial.println("Irrigando: ");
- lcd.print("Irrigando: ");
- lcd.print(Porcento);// Imprime um texto
- lcd.print("%"); // Imprime um texto
- digitalWrite(LED, HIGH);
- digitalWrite(porta_rele, HIGH);
-  }
- 
- else
- {
-  lcd.print("Umidade: ");
+/*--------------------------------------------------*/
+void mostrarDHT11(){ 
+    int chk = DHT11.read(DHT11PIN);
+    
+   lcd.print("Umidade Ar(%):");
+   lcd.print((float)DHT11.humidity, 2);
+   
+   lcd.setCursor(2, 1);
+   lcd.print("Temp Ar (C):");
+   lcd.print((float)DHT11.temperature, 2);
+}
+/*--------------------------------------------------*/
+void mostrarIrrigando(int Porcento){
+  lcd.print("Irrigando: ");
+  lcd.print(Porcento);// Imprime um texto
+  lcd.print("%"); // Imprime um texto
+  digitalWrite(LED, HIGH);
+  digitalWrite(porta_rele, HIGH);
+}
+/*--------------------------------------------------*/
+void mostrarUmidade(int Porcento){
+  lcd.print("Umidade Solo:");
   lcd.print(Porcento);
   lcd.print("%"); // Imprime um texto
- digitalWrite(LED, LOW);
- digitalWrite(porta_rele, LOW);
+  digitalWrite(LED, LOW);
+  digitalWrite(porta_rele, LOW);
+}
+/*--------------------------------------------------*/
+void loop(){
+ umidade = analogRead(A0);
+ int Porcento = map(umidade, 1023, 0, 0, 100);
+ lcd.clear();
+ mostrarDHT11();
+ delay(5000);
+ lcd.clear();
+ if(Porcento <=40){
+  mostrarIrrigando(Porcento);
+  }else{
+  mostrarUmidade(Porcento);
  }
+
+ delay(5000);
  
- delay(1000);
+ 
 }
